ebc4b873c4b6eeb9b597a9f811e0d23e9c588bcc
Kyotaro Kaneshima <e235727@ie.u-ryukyu.ac.jp>
2023-11-28 06:50:07 +0900

    option

--- app/src/main/java/jp/ac/uryukyu/ie/e235727/Warrior.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235727/Warrior.java
@@ -1,6 +1,6 @@
 package jp.ac.uryukyu.ie.e235727;
 
-public class Warrior extends LivingThing{
+public class Warrior extends Hero{
     public Warrior(String name, int maximumHP, int attack){
         super(name, maximumHP, attack);
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
@@ -16,13 +16,7 @@ public class Warrior extends LivingThing{
             opponent.wounded(noDamage);
         }
     }
-    public void wounded(int damage){
-        setHitPoint(getHitPoint() - damage);
-        if( getHitPoint() < 0){
-            setDead(true);
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n",getName());
-        }
-    }
+
 
 }
 
